2bc8b5f371b2926daa599909447773da90381d0c
1ike <googlohren@gmail.com>
2022-02-06 22:05:51 +0300

    Rename About page to Story page.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/story">Story</router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
 import Home from '../views/Home.vue';
+
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,12 +10,12 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/story',
+    name: 'Story',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (story.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "story" */ '../views/Story.vue'),
   },
 ];
 
