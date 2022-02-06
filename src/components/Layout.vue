b47aae4562ecb72d0edce33820ba32c73f3eb6d2
1ike <googlohren@gmail.com>
2022-02-06 23:38:31 +0300

    Add layout.

---
+++ src/components/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-layout>
+    <a-layout-header>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/story">Story</router-link>
+      </div>
+      <slot name="header"></slot>
+    </a-layout-header>
+    <a-layout-content>
+      <slot></slot>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Layout',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Layout>
+    <template v-slot:header> header slot </template>
+    <template v-slot:default>
+      <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+      </div>
+    </template>
+  </Layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Layout from '@/components/Layout.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
+    Layout,
     HelloWorld,
   },
 });
