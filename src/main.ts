ef3b4be1af83eda584c2f1e93a19e92b7aa53867
1ike <googlohren@gmail.com>
2022-02-09 01:21:07 +0300

    Bug when load icons.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { createApp } from 'vue';
 import { Inkline, components } from '@inkline/inkline';
 import '@inkline/inkline/inkline.scss';
+import { InklineIcons } from '@inkline/icons';
+// import { mdiAutorenew48 } from '@inkline/icons/packs/material-design';
 
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+
+InklineIcons.add({
+  // mdiAutorenew48,
+});
 
 createApp(App)
   .use(store)
@@ -13,4 +20,5 @@ createApp(App)
   .use(Inkline, {
     components,
   })
+  // .use(InklineIcons)
   .mount('#app');
