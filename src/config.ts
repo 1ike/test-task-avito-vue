f019f4d0efff91e7bfee28bad3ffadd89e1a9c26
1ike <googlohren@gmail.com>
2022-02-16 21:09:22 +0300

    Fix config: env vars names need prefix VUE_APP_.

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
+const int = (param: string) => parseInt(param, 10);
+
 export default {
   BASE_URL: process.env.BASE_URL,
-  HACKER_NEWS_API_URL: process.env.HACKER_NEWS_API_URL || 'https://hacker-news.firebaseio.com/v0/',
-  POLLING_INTERVAL: process.env.POLLING_INTERVAL || 60000,
-  STORIES_QTY: process.env.STORIES_QTY || 100,
-  STORIES_QTY_PER_PAGE: process.env.STORIES_QTY_PER_PAGE || 10,
-  TITLE_POSTFIX: process.env.TITLE_POSTFIX || 'Avito test task',
+  HACKER_NEWS_API_URL: process.env.VUE_APP_HACKER_NEWS_API_URL || 'https://hacker-news.firebaseio.com/v0/',
+  POLLING_INTERVAL: int(process.env.VUE_APP_POLLING_INTERVAL) || 6000,
+  STORIES_QTY: int(process.env.VUE_APP_STORIES_QTY) || 100,
+  STORIES_QTY_PER_PAGE: int(process.env.VUE_APP_STORIES_QTY_PER_PAGE) || 10,
+  TITLE_POSTFIX: process.env.VUE_APP_TITLE_POSTFIX || 'Avito test task',
 };
