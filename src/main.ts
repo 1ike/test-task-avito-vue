911c4a81500716504c3dfd8b3b184e1a8569dc8b
1ike <googlohren@gmail.com>
2022-02-16 21:24:37 +0300

    Remove InklineIcons.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 import { Inkline, components } from '@inkline/inkline';
 import '@inkline/inkline/inkline.scss';
-import { InklineIcons } from '@inkline/icons';
-import { mdiAutorenew48 } from '@inkline/icons/packs/material-design';
 
 import App from './App.vue';
 import './registerServiceWorker';
@@ -10,15 +8,10 @@ import router from './router';
 import store from './store';
 
 
-InklineIcons.add({
-  mdiAutorenew48,
-});
-
 createApp(App)
   .use(store)
   .use(router)
   .use(Inkline, {
     components,
   })
-  .use(InklineIcons)
   .mount('#app');
